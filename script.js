c101afc5e24b6c7f3efae29ad7183be374788c27
vitvinvaz <vitvinvaz111@gmail.com>
2025-01-23 16:14:16 +0530

    fix : changed height for menu

--- script.js
+++ script.js
@@ -3,18 +3,17 @@ const ul = document.getElementById("elements")
 const links = document.querySelectorAll("#header ul li")
 
 icon.addEventListener('click',() => {
-    console.log("Hamburger clicked!"); 
-    if (ul.style.left === "-100%" || ul.style.left === "") {
-        ul.style.left = "0"; 
+    if (ul.style.left === "100%" || ul.style.left === "") {
+        ul.style.left = "0";
     } else {
-        ul.style.left = "-100%"; 
+        ul.style.left = "100%";
     }
 })
 
 links.forEach(link => {
     link.addEventListener('click', (event) => {
         setTimeout(() => {
-            ul.style.left = "-100%";
+            ul.style.left = "100%";
         }, 200);
     });
 });
